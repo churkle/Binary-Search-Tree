8361f5b9e01ac996986d4d0d34dc5bb66bbf114a
Charlie Fei <charfei70@gmail.com>
2015-05-05 14:03:40 -0500

    Finished creating BinTree.h

--- Binary Search Tree/BinNode.h
+++ Binary Search Tree/BinNode.h
@@ -1,3 +1,9 @@
+// File name: BinNode.h
+// Author: Charles Fei
+// Project: Binary Search Tree
+// Date: 5/5/2014
+// Description: Header file for implementation of Binary Search Tree Node
+
 #ifndef BIN_NODE_H
 #define BIN_NODE_H
 
@@ -14,12 +20,18 @@ private:
 	ItemType item;
 	BinNodePtr left;
 	BinNodePtr right;
+	BinNodePtr findSuccessor() const;
 public:
 	BinNode(const ItemType & data); //ctor
 	BinNode(const BinNode & rhs); //copy ctor
 	~BinNode();
 	const BinNode & operator=(const BinNode& rhs);
 	void insert(const ItemType & data);
+	BinNodePtr remove(const ItemType& data) const;
+	bool search(const ItemType& data) const;
+	void printInOrder() const;
+	void printPreOrder() const;
+	void printPostOrder() const;
 };
 
 #endif
